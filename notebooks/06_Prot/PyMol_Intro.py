80f2095f4ca637ebb43ffb78ef11e55417be33a1
Tiago Antao <tiagoantao@gmail.com>
2015-03-01 19:59:29 +0000

    PyMol

--- notebooks/06_Prot/PyMol_Intro.py
+++ notebooks/06_Prot/PyMol_Intro.py
@@ -1,17 +1,19 @@
-import __main__
-#__main__.pymol_argv = [ 'pymol', '-qc'] # Quiet and no GUI
 import pymol
+pymol.pymol_argv = [ 'pymol', '-qc'] #  Quiet / no GUI
+from pymol import cmd
 pymol.finish_launching()
 
-pymol.cmd.fetch('1TUP')
-pymol.cmd.disable('all')
-pymol.cmd.enable('1TUP')
-#pymol.cmd.bg_color('white')
-pymol.cmd.png('/tmp/1TUP.png')
-pymol.cmd.hide('all')
-pymol.cmd.show('cartoon')
-#pymol.cmd.hide('cartoon', '/1TUP//E')
-#pymol.cmd.hide('cartoon', '/1TUP//F')
-#pymol.cmd.show('ribbon', '/1TUP//E')
-#pymol.cmd.show('ribbon', '/1TUP//F')
-#pymol.cmd.quit()
+cmd.fetch('1TUP', async=False)
+cmd.disable('all')
+cmd.enable('1TUP')
+#cmd.bg_color('white')
+cmd.hide('all')
+cmd.show('cartoon')
+cmd.hide('cartoon', '/1TUP//E')
+cmd.hide('cartoon', '/1TUP//F')
+cmd.show('ribbon', '/1TUP//E')
+cmd.show('ribbon', '/1TUP//F')
+cmd.select('zinc', 'name zn')
+cmd.show('sphere', 'zinc')
+cmd.png('1TUP.png', width=1980, height=1080, quiet=0, ray=1, prior=False)
+cmd.quit()
